20ff5c7d72dcca8219805c091cf92aade6abc4d7
Thomas W Bell <tom@tdm.co>
2013-05-24 18:20:56 +0100

    Fixed a problem with event propagation in the clickable viewheadertop div

--- js/scrolltop.js
+++ js/scrolltop.js
@@ -1,10 +1,15 @@
 $j(document).ready(function(){
 
 		// scroll body to 0px on click
-		$j('.viewheadertop').click(function () {
+		$j('.viewheadertop').click(function (e) {
 			$j('body,html').animate({
 				scrollTop: 0
 			}, 300);
 			return false;
 		});
+		$j(".viewheadertop a").click(function(e){
+    		e.stopPropagation();
+		});
 });
+});
+
